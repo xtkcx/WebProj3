29a610778a928c9511e185866cf6d6ed96ce0830
xtkcx <1117take@gmail.com>
2024-07-06 04:25:53 +0900

    HelloStruts機能

---
+++ src/com/diworksdev/webproj3/dto/HelloStrutsDTO.java
@@ -0,0 +1,10 @@
+package com.diworksdev.webproj3.dto;
+public class HelloStrutsDTO {
+	private String result;
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
